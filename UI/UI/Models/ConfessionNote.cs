c21708a2a7ec09d20c0b86ba01b990837252e39c
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2017-04-14 17:51:48 +0200

    change

--- UI/UI/Models/ConfessionNote.cs
+++ UI/UI/Models/ConfessionNote.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using UI.Abstractions;
 
 namespace UI.Models
 {
-    public class TodoItem : TableData
+    public class  ConfessionNote : TableData
     {
+        
+       
+        public static ObservableCollection<ConfessionNote> ConfessionsList = new ObservableCollection<ConfessionNote>();
         public string Text { get; set; }
         public string signature { get; set; }
         public string Categories { get; set; }
+
+        public ConfessionNote(string text1, string text2, string selecteditem)
+        {
+            this.Text = text1;
+            this.signature = text2;
+            this.Categories = selecteditem;
+        }
     }
 
+    
 
 
 }
