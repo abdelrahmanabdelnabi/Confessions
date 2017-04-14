d7944af215b46f4e30a19b1b84c5978f028820b9
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2017-04-14 17:50:24 +0200

    .cs

--- UI/UI/Pages/TaskDetails.xaml.cs
+++ UI/UI/Pages/TaskDetails.xaml.cs
@@ -1,4 +1,5 @@
-﻿using UI.Models;
+﻿using System.Threading.Tasks;
+using UI.Models;
 using UI.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,20 +9,21 @@ namespace UI.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TaskDetail : ContentPage
     {
-
+        string selecteditem;
         public TaskDetail()
         {
             InitializeComponent();
-           
 
 
-    }
+
+        }
         private void PickerCtl_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             if (PickerCtl != null && PickerCtl.SelectedIndex <= PickerCtl.Items.Count)
-     {
-                var selecteditem = PickerCtl.Items[PickerCtl.SelectedIndex];
+            {
+                selecteditem = PickerCtl.Items[PickerCtl.SelectedIndex];
                 DisplayAlert("Picker Control", selecteditem, "OK");
+
             }
         }
 
@@ -29,6 +31,41 @@ namespace UI.Pages
         {
             ConfessionNote note = new ConfessionNote(editor1.Text, SignatureName.Text, selecteditem);
 
-        }
+            Command cmdSave;
+
+        public Command SaveCommand => cmdSave ?? (cmdSave = new Command(async () => await ExecuteSaveCommand()));
+
+        async Task ExecuteSaveCommand()
+        {
+            if (IsBusy)
+                return;
+            IsBusy = true;
+
+            try
+            {
+                if (Item.Id == null)
+                {
+                    await table.CreateItemAsync(Item);
+                }
+                else
+                {
+                    await table.UpdateItemAsync(Item);
+                }
+                MessagingCenter.Send<TaskDetailViewModel>(this, "ItemsChanged");
+                await Application.Current.MainPage.Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[TaskDetail] Save error: {ex.Message}");
+                Debug.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        } }
     }
-}
+        
+
+  
+
