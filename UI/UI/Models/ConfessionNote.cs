8e103d07f84d3961ab580b7cfba6590e482e760d
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2017-04-14 16:42:16 +0200

    ConfessionNote

---
+++ UI/UI/Models/ConfessionNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UI.Abstractions;
+
+namespace UI.Models
+{
+    public class TodoItem : TableData
+    {
+        public string Text { get; set; }
+        public string signature { get; set; }
+        public string Categories { get; set; }
+    }
+
+
+
+}
